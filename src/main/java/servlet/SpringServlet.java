7449109a5c9a96b9a221672a9732b0c66037d70f
Tien Do Nam <dev.tien.donam@gmail.com>
2019-10-14 21:56:24 +0200

    renamed route /spring to /event

--- src/main/java/servlet/SpringServlet.java
+++ src/main/java/servlet/SpringServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.logging.Logger;
 
-@WebServlet("/spring")
+@WebServlet("/event")
 public class SpringServlet extends HttpServlet {
 
 	private static final Logger logger = Logger.getLogger(SpringServlet.class.getName());
